e11db1a4d0bb204fc934fdce231b4b0bc3971090
SouravBhattacharyya <sourav20089das@gmail.com>
2024-05-25 13:29:24 +0530

    Form Validation complete

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,35 +8,55 @@ import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
 import { DownloadCloud } from "lucide-react";
 import DarkMode from "./DarkMode";
+import { useForm } from "react-hook-form";
+import { validationScehma, validationScehmatype } from "@/utils/types/UserType";
+import { zodResolver } from "@hookform/resolvers/zod";
+import toast from "react-hot-toast";
 
 const Nav = () => {
+  // Form Validation
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<validationScehmatype>({
+    resolver: zodResolver(validationScehma),
+  });
+
+  const usernumberdata = (data: validationScehmatype) => {
+    console.log(data);
+    toast.error("Button Press Data Fetching incomplete");
+  };
+
   return (
     <>
-      <Navbar
-        maxWidth="xl"
-        className="h-[12vh]"
-        // isBlurred
-        // isBordered
-        // isMenuOpen={isMenuOpen}
-        // onMenuOpenChange={setIsMenuOpen}
-      >
+      <Navbar maxWidth="xl" className="h-[12vh]">
         <NavbarBrand className="text-2xl font-bold">RandomUser V2</NavbarBrand>
         <NavbarContent justify="end" className=" hidden sm:flex">
           <NavbarItem>
-            <form className="flex gap-2" noValidate>
+            <form
+              className="flex gap-2"
+              noValidate
+              onSubmit={handleSubmit(usernumberdata)}
+            >
               <Input
                 type="number"
                 defaultValue="3"
                 label="Enter Number"
                 labelPlacement="outside-left"
+                {...register("userNumber")}
+                errorMessage={errors.userNumber?.message}
+                isInvalid={errors.userNumber?.message ? true : false}
               />
+
               <Button
                 type="submit"
                 color="secondary"
                 variant="shadow"
                 endContent={<DownloadCloud />}
               >
-                Get
+                {isSubmitting ? "" : "Get"}
               </Button>
             </form>
           </NavbarItem>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import DisplayCards from "@/components/DisplayCards";
 import Nav from "@/components/Nav";
+import Head from "next/head";
+import { Toaster } from "react-hot-toast";
 
 const index = () => {
   return (
     <>
+      <Head>
+        <title>RandomUser App</title>
+      </Head>
       <div className="container mx-auto">
         <Nav />
         <DisplayCards />
       </div>
+      <Toaster position="top-center" reverseOrder={false} />
     </>
   );
 };
